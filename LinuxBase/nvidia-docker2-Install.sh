9f0ac43fb9f1adf153f4d93c98c3421225023c77
Rieguass <Danfeng5.wang@gmail.com>
2020-01-09 14:22:43 +0800

    update

---
+++ LinuxBase/nvidia-docker2-Install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
+  sudo apt-key add -
+ 
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+ 
+curl -s -L https://nvidia.github.io/nvidia-docker/ubuntu16.04/nvidia-docker.list | \
+  sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update
+sudo apt-get install -y nvidia-docker2
